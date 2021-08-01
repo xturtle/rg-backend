eb940ce045bef5af7d03d7182a4c7a95bc5390f3
William Tsai <the.dragonblue@gmail.com>
2021-08-01 19:24:42 +0800

    remove unnecessary migration; install package first

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@
 #npx sequelize-cli model:generate --name User --attributes username:string,password:string --force
 
 sudo npm i -g pm2 cross-env
-cross-env NODE_ENV=production npx sequelize-cli db:migrate:undo:all && npm run migrate
+cd src/
+cross-env NODE_ENV=production npx sequelize-cli db:migrate:undo:all
+cross-env NODE_ENV=production npm run migrate
+cd ../
 mkdir web
 npm run build
 cross-env NODE_ENV=production pm2 start ./bin
