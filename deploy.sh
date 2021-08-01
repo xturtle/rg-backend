2a0e68e7d69a10af9ed09e99e8953fac036d3e60
William Tsai <the.dragonblue@gmail.com>
2021-08-01 19:29:50 +0800

    fix deploy.sh: when production db exists, do not wipe it

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 #npx sequelize-cli model:generate --name Post --attributes uid:integer,image:string,text:string
 #npx sequelize-cli model:generate --name User --attributes username:string,password:string --force
 
+npm install
 sudo npm i -g pm2 cross-env
 cd src/
-cross-env NODE_ENV=production npx sequelize-cli db:migrate:undo:all
-cross-env NODE_ENV=production npm run migrate
+if [ ! -f ../database.sqlite ]; then
+    cross-env NODE_ENV=production npx sequelize-cli db:migrate
+fi
 cd ../
 mkdir web
 npm run build
