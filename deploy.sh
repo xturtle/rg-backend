0e16ee705ed382a06b0fbbffe390973158b4e559
William Tsai <the.dragonblue@gmail.com>
2021-08-01 19:19:20 +0800

    add pm2 for production config

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #node > v14.17.3
-npx sequelize-cli model:generate --name Post --attributes uid:integer,image:string,text:string --force
-npx sequelize-cli model:generate --name User --attributes username:string,password:string --force
+#npx sequelize-cli model:generate --name Post --attributes uid:integer,image:string,text:string
+#npx sequelize-cli model:generate --name User --attributes username:string,password:string --force
+
+sudo npm i -g pm2 cross-env
+cross-env NODE_ENV=production npx sequelize-cli db:migrate:undo:all && npm run migrate
+mkdir web
+npm run build
+cross-env NODE_ENV=production pm2 start ./bin
